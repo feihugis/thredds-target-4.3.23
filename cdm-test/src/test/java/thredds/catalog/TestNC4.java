d31bdbf620a0de1e93d26339f997b079196a13a1
Fei Hu <hufei68@gmail.com>
2016-02-14 01:11:26 -0500

    Get Meta Info for NetCDF4/HDF5
    Get data chunk info

--- cdm-test/src/test/java/thredds/catalog/TestNC4.java
+++ cdm-test/src/test/java/thredds/catalog/TestNC4.java
@@ -17,24 +17,26 @@ import ucar.unidata.io.RandomAccessFile;
 public class TestNC4 {
 
   public static void main(String[] args) throws IOException, InvalidRangeException {
+    //String fPath = "/Users/feihu/Documents/Data/Merra/MERRA300.prod.simul.tavgM_2d_mld_Nx.201306.hdf";
+    String fPath = "/Users/feihu/Documents/Data/Merra2/MERRA2_100.inst1_2d_int_Nx.19800101.nc4";
 
-    RandomAccessFile randomAccessFile = new RandomAccessFile("/Users/feihu/Documents/Data/Merra2/MERRA2_100.inst1_2d_int_Nx.19800101.nc4",
-                                                             "rws");
+    RandomAccessFile randomAccessFile = new RandomAccessFile(fPath, "rws");
     randomAccessFile.setDebugAccess(true);
 
-    NetcdfFile ncfile = NetcdfFile.open(randomAccessFile, "/Users/feihu/Documents/Data/Merra2/MERRA2_100.inst1_2d_int_Nx.19800101.nc4");
+    NetcdfFile ncfile = NetcdfFile.open(randomAccessFile, fPath);
 
     //System.out.println(ncfile.getDetailInfo());
     List<Variable> varList = ncfile.getVariables();
     //for (Variable var : varList) {
     Variable var = varList.get(10);
     int[] start = new int[] {0,0,0};
-    int[] shape = new int[] {10,100,100};
-    H5header.Vinfo vinfo = (H5header.Vinfo) var.getSPobject();
-    int[] tmp = vinfo.getChunking();
+    int[] shape = new int[] {1,1,1};
+    //H5header.Vinfo vinfo = (H5header.Vinfo) var.getSPobject();
+    //int[] tmp = vinfo.getChunking();
     Array value = var.read();
     value.getShape();
-    System.out.println(var.getShortName()+ vinfo.getChunking().length + "+++++++++" + var.read(start, shape).getSize() + " , " + value.getShape()[0]);
+    //var.getVarLocationInformation();
+    System.out.println(var.getShortName()+ "+++++++++" + var.read(start, shape).getSize() + " , " + var.getVarLocationInformation());
     //}
   }
 
